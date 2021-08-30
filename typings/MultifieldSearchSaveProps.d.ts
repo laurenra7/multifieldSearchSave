fa12e959921aaa71020d72c269c4d30c49df3cae
Lauren Anderson <laurenra7@yahoo.com>
2021-08-30 14:42:02 -0600

    Updated XML with widget properties copied from GridSearch Multifield and search save entity and attribute

--- typings/MultifieldSearchSaveProps.d.ts
+++ typings/MultifieldSearchSaveProps.d.ts
@@ -4,6 +4,26 @@
  * @author Mendix UI Content Team
  */
 import { CSSProperties } from "react";
+import { EditableValue, ListValue, ListAttributeValue } from "mendix";
+import { Big } from "big.js";
+
+export type SearchMethodParamEnum = "startswith" | "contains";
+
+export interface SearchAttributesType {
+    searchAttributeParam?: EditableValue<string | Big>;
+    searchMethodParam: SearchMethodParamEnum;
+    customSearchEntity?: ListValue;
+    customSearchAttribute?: ListAttributeValue<string | Big>;
+    customSearchPath: string;
+}
+
+export interface SearchAttributesPreviewType {
+    searchAttributeParam: string;
+    searchMethodParam: SearchMethodParamEnum;
+    customSearchEntity: {} | { type: string } | null;
+    customSearchAttribute: string;
+    customSearchPath: string;
+}
 
 export interface MultifieldSearchSaveContainerProps {
     name: string;
@@ -11,10 +31,38 @@ export interface MultifieldSearchSaveContainerProps {
     style?: CSSProperties;
     tabIndex?: number;
     sampleText: string;
+    targetGridName: string;
+    targetGridClass: string;
+    renderAsTextarea: boolean;
+    ta_rows: number;
+    filterLabel: string;
+    gridEntity: ListValue;
+    minCharacters: string;
+    searchAttributes: SearchAttributesType[];
+    expertQuery: string;
+    allowOrConditions: boolean;
+    useRegEx: boolean;
+    splitString: string;
+    searchSaveEntity?: ListValue;
+    searchSaveAttribute?: ListAttributeValue<string | Big | Date>;
 }
 
 export interface MultifieldSearchSavePreviewProps {
     class: string;
     style: string;
     sampleText: string;
+    targetGridName: string;
+    targetGridClass: string;
+    renderAsTextarea: boolean;
+    ta_rows: number | null;
+    filterLabel: string;
+    gridEntity: {} | { type: string } | null;
+    minCharacters: string;
+    searchAttributes: SearchAttributesPreviewType[];
+    expertQuery: string;
+    allowOrConditions: boolean;
+    useRegEx: boolean;
+    splitString: string;
+    searchSaveEntity: {} | { type: string } | null;
+    searchSaveAttribute: string;
 }
